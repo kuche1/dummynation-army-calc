bb094405e77a8cc84d172b13db66f62f900c02d8
kuche1 <znamsiparolata4esno@abv.bg>
2025-12-28 14:59:05 +0200

    update

--- dummynation-army-calc.py
+++ dummynation-army-calc.py
@@ -1,30 +1,55 @@
 #! /usr/bin/env python3
 
 from argparse import ArgumentParser
+from itertools import product
 
-from troop import ALL_TROOPS, Troop
+from troop import ALL_TROOPS, Troop, combine_troops
 
 # LIMIT_RECRUITMENT_COST = 5_000_000
 
 
 def main() -> None:
     # find_best_single_troop(ALL_TROOPS)
-    find_best_combination_of_2_troops(ALL_TROOPS)
+    # find_best_combination_of_2_troops(ALL_TROOPS)
+    find_best_combination_of_multiple_troops(ALL_TROOPS)
 
 
-def find_best_combination_of_2_troops(all_troops_as_set: set[Troop]) -> None:
-    all_troops = list(all_troops_as_set)
+# TODO: find best combination of multiple troops, given that you can use each as many times as you want
+# currenty you are limited to 6 times each troop OR 5 times first and 1 time second OR 4 times first and 2 times second AND SO ON
 
+
+def find_best_combination_of_multiple_troops(all_troops: set[Troop]) -> None:
     mixed_troops: set[Troop] = set()
 
-    for troop_a_idx, troop_a in enumerate(all_troops):
-        for troop_b in all_troops[troop_a_idx:]:
-            new_troop = troop_a + troop_b
-            # print(f"{new_troop=}")
-            # print()
+    for combination in product(*([ALL_TROOPS] * len(ALL_TROOPS))):
+        # print(f"{combination=}")
+        new_troop = combine_troops(combination)
+
+        do_add = True
+
+        for registered_troop in mixed_troops:
+            if registered_troop.name == new_troop.name:
+                do_add = False
+
+        if do_add:
             mixed_troops.add(new_troop)
 
     find_best_single_troop(mixed_troops)
+
+
+# def find_best_combination_of_2_troops(all_troops_as_set: set[Troop]) -> None:
+#     all_troops = list(all_troops_as_set)
+
+#     mixed_troops: set[Troop] = set()
+
+#     for troop_a_idx, troop_a in enumerate(all_troops):
+#         for troop_b in all_troops[troop_a_idx:]:
+#             new_troop = troop_a + troop_b
+#             # print(f"{new_troop=}")
+#             # print()
+#             mixed_troops.add(new_troop)
+
+#     find_best_single_troop(mixed_troops)
 
 
 def find_best_single_troop(all_troops: set[Troop]) -> None:
--- troop.py
+++ troop.py
@@ -33,23 +33,23 @@ class Troop:
     def __repr__(self) -> str:
         return f"{self.name}:\n    military_personel: {self.military_personel:_.1f}\n    recruitment_cost: {self.recruitment_cost:_.1f}\n    maintenance_cost: {self.maintenance_cost:_.1f}\n    attack: {self.attack:_.1f}\n    defense: {self.defense:_.1f}\n    pierce: {self.pierce:_.1f}\n    speed: {self.speed}"
 
-    def __add__(self, other: "Troop") -> "Troop":
-        # this is only good enough for adding 2 troops together, not 3 or more
-        if self.name < other.name:
-            name = f"{self.name} + {other.name}"
-        else:
-            name = f"{other.name} + {self.name}"
+    # def __add__(self, other: "Troop") -> "Troop":
+    #     # this is only good enough for adding 2 troops together, not 3 or more
+    #     if self.name < other.name:
+    #         name = f"{self.name} + {other.name}"
+    #     else:
+    #         name = f"{other.name} + {self.name}"
 
-        return Troop(
-            name,
-            (self.military_personel + other.military_personel) / 2,
-            (self.recruitment_cost + other.recruitment_cost) / 2,
-            (self.maintenance_cost + other.maintenance_cost) / 2,
-            (self.attack + other.attack) / 2,
-            (self.defense + other.defense) / 2,
-            (self.pierce + other.pierce) / 2,
-            min(self.speed, other.speed),
-        )
+    #     return Troop(
+    #         name,
+    #         (self.military_personel + other.military_personel) / 2,
+    #         (self.recruitment_cost + other.recruitment_cost) / 2,
+    #         (self.maintenance_cost + other.maintenance_cost) / 2,
+    #         (self.attack + other.attack) / 2,
+    #         (self.defense + other.defense) / 2,
+    #         (self.pierce + other.pierce) / 2,
+    #         min(self.speed, other.speed),
+    #     )
 
 
 COMMANDO = Troop(
@@ -119,3 +119,20 @@ COMBAT_AIRCRAFT = Troop(
 )
 
 ALL_TROOPS = {COMMANDO, GUNNER, TANK, ROCKET_ARTILLERY, HELICOPTER, COMBAT_AIRCRAFT}
+
+
+def combine_troops(troops: tuple[Troop, ...]) -> Troop:
+    all_names = [troop.name for troop in troops]
+    all_names.sort()
+    name = " + ".join(all_names)
+
+    return Troop(
+        name,
+        sum([troop.military_personel for troop in troops]) / len(troops),
+        sum([troop.recruitment_cost for troop in troops]) / len(troops),
+        sum([troop.maintenance_cost for troop in troops]) / len(troops),
+        sum([troop.attack for troop in troops]) / len(troops),
+        sum([troop.defense for troop in troops]) / len(troops),
+        sum([troop.pierce for troop in troops]) / len(troops),
+        min([troop.speed for troop in troops]),
+    )
