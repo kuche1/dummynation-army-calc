22c847f4b2f85d3e9c8e2e162fcdfdd97cee742e
kuche1 <znamsiparolata4esno@abv.bg>
2025-12-28 14:22:15 +0200

    update

--- dummynation-army-calc.py
+++ dummynation-army-calc.py
@@ -1,3 +1,21 @@
 #! /usr/bin/env python3
 
+from argparse import ArgumentParser
+
 import troop
+
+print(f"{troop.COMMANDO=}")
+
+
+def main() -> None:
+    pass
+
+
+def run_from_cmdline() -> None:
+    parser = ArgumentParser()
+    _args = parser.parse_args()
+    main()
+
+
+if __name__ == "__main__":
+    run_from_cmdline()
--- troop.py
+++ troop.py
@@ -1,3 +1,9 @@
+_K = 1_000
+_M = _K * 1_000
+
+_TARGET_MAINTNANCE_COST = 1 * _M
+
+
 class _Troop:
     def __init__(
         self,
@@ -9,70 +15,80 @@ class _Troop:
         pierce: float,
         speed: int,
     ) -> None:
-        self.military_personel = military_personel
-        self.recruitment_cost = recruitment_cost
-        self.maintenance_cost = maintenance_cost
+        multiplier = _TARGET_MAINTNANCE_COST / maintenance_cost
 
+        self.military_personel = multiplier * military_personel
+        self.recruitment_cost = multiplier * recruitment_cost
+        self.maintenance_cost = multiplier * maintenance_cost
 
-k = 1_000
-m = k * 1_000
+        self.attack = multiplier * attack
+        self.defense = multiplier * defense
+        self.pierce = multiplier * pierce
+
+        self.speed = speed
+
+    def __repr__(self) -> str:
+        return f"Troop:\n    military_personel: {self.military_personel:_.1f}\n    recruitment_cost: {self.recruitment_cost:_.1f}\n    maintenance_cost: {self.maintenance_cost:_.1f}\n    attack: {self.attack:_.1f}\n    defense: {self.defense:_.1f}\n    pierce: {self.pierce:_.1f}\n    speed: {self.speed}"
+
 
 COMMANDO = _Troop(
-    1 * k,
-    6.35 * k,
+    1 * _K,
+    6.35 * _K,
     403.73,
-    15 * k,
-    5.55 * k,
-    4 * k,
+    15 * _K,
+    5.55 * _K,
+    4 * _K,
     10,
 )
 
 GUNNER = _Troop(
-    1 * k,
-    9.49 * k,
+    1 * _K,
+    9.49 * _K,
     603.68,
-    12 * k,
-    2.22 * k,
-    19 * k,
+    12 * _K,
+    2.22 * _K,
+    19 * _K,
     8,
 )
 
 TANK = _Troop(
     70,
-    14.2 * k,
+    14.2 * _K,
     140.86,
-    7.7 * k,
-    19.42 * k,
-    3.5 * k,
+    7.7 * _K,
+    19.42 * _K,
+    3.5 * _K,
     5,
 )
 
 ROCKET_ARTILERY = _Troop(
     225,
-    12.26 * k,
+    12.26 * _K,
     241.01,
     900,
-    5.99 * k,
-    22.5 * k,
+    5.99 * _K,
+    22.5 * _K,
     6,
 )
 
 HELICOPTER = _Troop(
     25,
-    11.27 * k,
+    11.27 * _K,
     167.16,
-    25 * k,
-    2.22 * k,
+    25 * _K,
+    2.22 * _K,
     500,
     16,
 )
 
 COMBAT_AIRCRAFT = _Troop(
     4,
-    12.12 * k,
-    106.98 * k,
-    6 * k,
-    11.1 * k,
-    8 * k,
+    12.12 * _K,
+    106.98 * _K,
+    6 * _K,
+    11.1 * _K,
+    8 * _K,
     20,
 )
+
+ALL_TROOPS = {COMMANDO, GUNNER, TANK, ROCKET_ARTILERY, HELICOPTER, COMBAT_AIRCRAFT}
